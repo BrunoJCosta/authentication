82f4af3989486cde5ff45b64440c36f52641536d
BrunoJCosta <brunojereissati00@gmail.com>
2024-08-10 13:35:22 -0300

    created first authentication for UserDetail

--- src/main/java/br/com/login/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/login/configuration/SecurityConfiguration.java
@@ -1,17 +1,14 @@
 package br.com.login.configuration;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -20,9 +17,6 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableWebSecurity // this annotation disable all configuration of spring
 @EnableMethodSecurity(jsr250Enabled = true) // autoriza os PreAutorized (Permissões) no controller
 public class SecurityConfiguration {
-
-//    private final PasswordEncoder passwordEncoder;
-
 
     public static final String[] GET_PUBLICOS = {
             "/test"
@@ -35,7 +29,6 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 /* This csrf disable because this project it doesn't work with jsp (back and front)
                 * It doesn't need use past authentication in all as requisition
-                * With it disable the default spring configuration is only get work
                  */
                 .httpBasic(Customizer.withDefaults())
                 .addFilterBefore(customFilter, BasicAuthenticationFilter.class)
--- src/main/java/br/com/login/configuration/UserService.java
+++ src/main/java/br/com/login/configuration/UserService.java
@@ -3,9 +3,11 @@ package br.com.login.configuration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+@Service
 public class UserService implements UserDetailsService {
 
     @Override
