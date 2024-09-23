63e34f6ad64ae2c5b6dfd639e0ddb9b8a4b2f061
BrunoJCosta <brunojereissati00@gmail.com>
2024-09-23 20:41:45 -0300

    created the login

--- src/main/java/br/com/login/configuration/UserDTO.java
+++ src/main/java/br/com/login/configuration/UserDTO.java
@@ -14,12 +14,4 @@ public class UserDTO implements UserDetails {
     private String username;
     private List<Permission> authorities;
 
-    public static UserDTO montarDTO() {
-        UserDTO dto = new UserDTO();
-        dto.authorities = List.of(Permission.USER_COMUM);
-        dto.username = "user";
-        // password: pass
-        dto.password = "$2a$12$M4f0mNHtFcXTcDpW/SOsg.lVyMVZ4.SMMGiMWHV3h37HGsS3eXeB.";
-        return dto;
-    }
 }
