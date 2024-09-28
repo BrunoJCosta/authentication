43effa074a7401da62eead92a8853adede07e1fc
Bruno <brunojereissati@edu.unifor.br>
2024-09-28 09:56:51 -0300

    configuration the in project with h2

---
+++ src/test/java/br/com/login/FirstTest.java
@@ -0,0 +1,19 @@
+package br.com.login;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class FirstTest {
+
+    @Test
+    @SuppressWarnings("JUnit5AssertionsConverter")
+    void testing() {
+        Assert.assertTrue(true);
+    }
+
+}
