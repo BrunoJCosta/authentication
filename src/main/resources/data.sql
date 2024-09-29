75de07ac455580745f0502392ca7448a00d6aa8a
Bruno <brunojereissati@edu.unifor.br>
2024-09-28 22:33:37 -0300

    puting data.sql with default values in entity users and personal_data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- this cpf is a random, i get in forDev
+insert into users.personal_data(pk, cpf, genero, name)
+values (1,'34164480068', 'M', 'Bruno Jereissati da Costa')
+on conflict do nothing;
+
+-- password: bruno@123
+insert into users.users(pk, email, password, date_created, personal_data_id)
+VALUES (1,'bruno@gmail.com',
+        '$2a$12$yKL2i.1u/IbkiDgDmDjt.u5oetaYyXtwtdoSxFVhp2E3nNk3iroq.',
+        '2024-09-28 21:52:00', 1)
+on conflict do nothing;
