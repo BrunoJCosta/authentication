14485d7858c0203fbfbd5a4eb7b2a7b19bb5fbc7
Bruno <brunojereissati@edu.unifor.br>
2024-10-19 09:39:48 -0300

    Rotes' improving

--- src/main/java/br/com/login/controller/AuthenticationController.java
+++ src/main/java/br/com/login/controller/AuthenticationController.java
@@ -1,27 +1,24 @@
 package br.com.login.controller;
 
-import br.com.login.configuration.UserDTO;
+import br.com.login.configuration.token.TokenForm;
 import br.com.login.configuration.token.TokenService;
 import br.com.login.users.EntityUserServices;
 import br.com.login.users.LoginDTO;
 import br.com.login.users.LoginFom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@CrossOrigin
+@RequestMapping("/authentication")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final EntityUserServices userServices;
     private final TokenService tokenService;
 
-    @PostMapping
+    @PostMapping("/auth")
     public ResponseEntity<LoginDTO> login(@RequestBody LoginFom fom) throws Exception {
         try {
             LoginDTO loginDTO = userServices.findByEmailUserDTO(fom.email(), fom.password());
