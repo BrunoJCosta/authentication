bc101af3e3bc2ef9eb971574e776909cf6884285
bruno <brunojereissati@edu.unifor.br>
2024-11-22 17:54:20 -0300

    feat(util): putting putMask in CpfUtils

--- src/main/java/br/com/login/utils/CpfUtils.java
+++ src/main/java/br/com/login/utils/CpfUtils.java
@@ -2,8 +2,33 @@ package br.com.login.utils;
 
 public class CpfUtils {
 
-    public static String removeCpfMask(String cpf) {
+    private static final int length_without_mask = 11;
+    private static final int length_with_mask = 14;
+
+    public static String removeMask(String cpf) {
         return StringUtils.empty(cpf) ? "" : cpf.replaceAll("[.-]","");
+    }
+
+    public static String putMask(String cpf) {
+        if (StringUtils.empty(cpf))
+            return "";
+
+        if (cpf.length() == length_with_mask)
+            return cpf;
+
+        if (cpf.length() != length_without_mask)
+            return "";
+
+        String first = cpf.substring(0, 3);
+        String second = cpf.substring(3, 6);
+        String threeth = cpf.substring(6, 9);
+        String fourth = cpf.substring(9, 11);
+
+        return first + "." + second + "." + threeth + "-" + fourth;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(putMask("07659045350"));
     }
 
 }
