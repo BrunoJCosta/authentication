66056e745457979738de7898d327fe4b13def46c
Bruno <brunojereissati@edu.unifor.br>
2024-11-21 21:44:09 -0300

    created my generic exception

---
+++ src/main/java/br/com/login/exception/AuthException.java
@@ -0,0 +1,11 @@
+package br.com.login.exception;
+
+public class AuthException extends Exception {
+
+    public AuthException(String message) {
+        super(message);
+    }
+
+    public AuthException() {
+    }
+}
