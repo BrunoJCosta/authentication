2892fae52e64993b11a10dd48dd516788dcc960b
Bruno <brunojereissati@edu.unifor.br>
2024-11-25 21:14:25 -0300

    routine the save the userAdmin when upload the project

--- src/main/java/br/com/login/configuration/RoutineUserAdmin.java
+++ src/main/java/br/com/login/configuration/RoutineUserAdmin.java
@@ -4,6 +4,7 @@ import br.com.login.users.EntityUserServices;
 import br.com.login.users.ProfileDTO;
 import br.com.login.users.UserForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -16,18 +17,21 @@ public class RoutineUserAdmin {
     private final EntityUserServices entityUserService;
 
     @Bean
-    public void createdUser() throws Exception {
-        String email = "bruno@gmail.com";
-        Optional<ProfileDTO> userAdmin = entityUserService.findByEmailUserDTO(email);
-        if (userAdmin.isPresent())
-            return;
+    public ApplicationRunner createdUser() {
+        return args -> {
+            String email = "bruno@gmail.com";
+            Optional<ProfileDTO> userAdmin = entityUserService.findByEmailUserDTO(email);
+            if (userAdmin.isPresent()) {
+                return;
+            }
 
-        String nome = "Bruno jereissati da Costa";
-        String cpf = "34164480068";
-        String gender = "Masculine";
-        String senha = "bruno@123";
-        UserForm form = new UserForm(email, nome, cpf, gender, senha, senha);
-        entityUserService.createdAutomatic(form);
-
+            String nome = "Bruno jereissati da Costa";
+            String cpf = "34164480068";
+            String gender = "Masculine";
+            String senha = "bruno@123";
+            UserForm form = new UserForm(email, nome, cpf, gender, senha, senha);
+            entityUserService.createdAutomatic(form);
+        };
     }
+
 }
